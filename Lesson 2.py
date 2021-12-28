039dedd8e58abf4ea8f4810989eff82ceca1efee
AndreyKozlov1003 <andreykozlov10031994@gmail.com>
2021-12-28 18:14:53 +0500

    Update Lesson 2.py

--- Lesson 2.py
+++ Lesson 2.py
@@ -24,7 +24,7 @@ for i in range(0,len(s)-1,2):
 print('Перемешанный список - {}'.format(s))'''
 
 #задание 3
-mes = {'зима' : [12,1,2],
+'''mes = {'зима' : [12,1,2],
        'весна' : [3,4,5],
        'лето' : [6,7,8],
        'осень' : [9,10,11]   
@@ -35,7 +35,17 @@ state = list(enumerate(state))
 number = int(input('Введите номер месяца, а я скажу, какой это сезон года - '))
 for i in range (0,len(num)):
     if num[i].count(number) != 0:
-        print('Сейчас - {}'.format(state[i][1]))
+        print('Сейчас - {}'.format(state[i][1]))'''
+        
+#задание 4 
+input_str = input ('Введите несколько слов, разделенные пробелами - ')
+words = input_str.split()
+for i in range(0,len(words)):
+    if len(words[i]) > 10:
+        print('{}. {}'.format(i+1,words[i][0:10]))
+    else:
+        print('{}. {}'.format(i+1,words[i]))
+
 
 
 
